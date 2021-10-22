80387eeca82751edd37bd757222c895f6151e43e
unknown <dtulpa16@yahoo.com>
2021-10-22 11:20:15 -0500

    Added Delete Method To Todo List

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -13,3 +13,8 @@ def index (request):
     return render(request, 'index.html', {'todos': todo})
 
 # Create your views here.
+
+def delete(request,pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
